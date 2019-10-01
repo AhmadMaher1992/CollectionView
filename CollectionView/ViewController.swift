9132cde832442fad6e54e7aa9e763fdf477e73b7
Ahmad Maher <ahmadeisa1992@gmail.com>
2019-09-30 19:54:49 -0700

    Initial Commit

---
+++ CollectionView/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  CollectionView
+//
+//  Created by mac on 9/30/19.
+//  Copyright © 2019 mac. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
